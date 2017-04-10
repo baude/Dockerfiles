05d7c402211cd7dea22511fc2f1bc8b808bda57e
Brent Baude <bbaude@redhat.com>
2017-04-10 13:15:04 -0500

    Updates for dnf and rpm-build on Fedora

--- fedora_builder/watch.sh
+++ fedora_builder/watch.sh
@@ -9,7 +9,7 @@ FILE=`echo $OUTPUT | awk '{print $2}'`
   if [[ $FILE == *\.src\.rpm ]]; then
 	  if [[ $ACTION == "CLOSE_WRITE,CLOSE" ]]; then
 		  echo "Building $FILE"
-		  yum-builddep -y /rpms/$FILE
+		  dnf builddep -y /rpms/$FILE
 		  rpmbuild --rebuild --define "_rpmdir /rpms/out/" /rpms/$FILE
 		  echo "+++++++++++++++++++++++++++++++++++++++"
 		  echo "Done building $FILE"
